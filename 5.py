ecbefa09816e2cc1a33496c3065757af09d03007
Shivam Ravikant Kale <shivamrkale1619@gmail.com>
2025-09-05 20:33:38 +0530

    add this in python

---
+++ 5.py
@@ -0,0 +1,7 @@
+# Membership Operators
+fruits = ["apple", "banana", "cherry"]
+if "banana" in fruits:
+    print("Banana is in the list of fruits.")  # Output: Banana is in the list of fruits.
+
+if "grape" not in fruits:
+    print("Grape is not in the list of fruits.")  # Output: Grape is not in the list of fruits.
